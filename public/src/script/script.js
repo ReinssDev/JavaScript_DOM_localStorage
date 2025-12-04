f6390a446307064d6cc5d8f72264bd90ac6b198f
Dimas <Dimasiryantosunandar710@gmail.com>
2025-12-04 09:43:45 +0700

    commit pertama

---
+++ public/src/script/script.js
@@ -0,0 +1,58 @@
+let root = document.getElementById('root');
+const btnLogin = document.getElementById('btn-login');
+const usernameInput = document.getElementById('usernameInput');
+const passwordInput = document.getElementById('passwordInput');
+const admin = document.getElementById('admin');
+const user = document.getElementById('user');
+
+const onLogin = () => {
+    console.log('Tombol login di klik');
+    console.log(usernameInput.value);
+    console.log(passwordInput.value);
+    localStorage.setItem('username', usernameInput.value);
+
+    if(usernameInput.value === 'admin' && passwordInput.value === 'admin123#') {
+        alert('Anda login sebagai admin');
+        const textAdmin = 'Hai, admin. Semoga hari ini anda bisa menjalani aktivitas dengan baik!';
+        admin.append(textAdmin);
+        localStorage.setItem('role', 'admin');
+        usernameInput.style.display = 'none';
+        passwordInput.style.display = 'none';
+        btnLogin.style.display = 'none';
+    } else {
+        alert('Anda login sebagai user biasa');
+        const textUser = `Hai, ${usernameInput.value} semoga hari ini menjadi hari yang menyenangkan`;
+        user.append(textUser);
+        localStorage.setItem('role', 'user');
+        usernameInput.style.display = 'none';
+        passwordInput.style.display = 'none';
+        btnLogin.style.display = 'none';
+    }
+
+}
+
+if(localStorage.getItem('username')) {
+    // usernameInput.style.display = 'none';
+    // passwordInput.style.display = 'none';
+    // btnLogin.style.display = 'none';
+
+    if(localStorage.getItem('role') === 'admin') {
+        const textAdmin = 'Hai, admin. Semoga hari ini anda bisa menjalani aktivitas dengan baik!';
+        admin.append(textAdmin);
+        user.style.display = 'none';
+        // usernameInput.style.display = 'none';
+        // passwordInput.style.display = 'none';
+        // btnLogin.style.display = 'none';
+    } else {
+        const textUser = `Hai, ${usernameInput.value} semoga hari ini menjadi hari yang menyenangkan`;
+        user.append(textUser);
+        admin.style.display = 'none';
+        // usernameInput.style.display = 'none';
+        // passwordInput.style.display = 'none';
+        // btnLogin.style.display = 'none';
+    }
+}
+
+btnLogin.addEventListener('click', () => {
+    onLogin();
+});
